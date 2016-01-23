e6f13b4f3532822bcb2a1028a3e8e9043514172b
Marcelo Boeira <me@marceloboeira.com>
2016-01-23 02:41:39 -0200

    feature: Show current pane version instead of global

--- scripts/ruby_version.sh
+++ scripts/ruby_version.sh
@@ -4,7 +4,10 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/helpers.sh"
 
+target_path= $(get_tmux_options "pane_current_path")
+
 print_ruby_version() {
+  cd $target_path
   if command_exists "rbenv"; then
     rbenv version-name
   fi
